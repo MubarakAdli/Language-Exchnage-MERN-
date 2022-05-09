f2f18cb1bcb3389a42b5f33bcce3ea7eb25949e3
hassano1696 <hassanodeh1997@gmail.com>
2022-05-09 15:16:22 +0300

    roomcrud

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -1,18 +1,46 @@
 const {Room} = require("../models/room.model");
-const { request, response } = require('express');
 
 
 
 class RoomController {
 
-
-  create(request, response) {
-    const {lang1,lang2,desc} = request.body;
+  getAll = (req, res) => {
+    Player.find({})
+      .then((rooms) => res.json(rooms))
+      .catch((err) => res.status(400).json(err));
+  };
+  
+  create(req, res) {
+    const {lang1,lang2,desc} = req.body;
     Room.create({lang1,lang2,desc})
-      .then(newRoom => response.json(newRoom)
-      ,console.log("hh"))
-      .catch(err => response.json(err));
+      .then(newRoom => res.json(newRoom)
+      )
+      .catch(err => res.json(err));
   }
+
+  delete = (req, res) => {
+    const { id } = req.params;
+    Room.deleteOne({
+      _id: id,
+    })
+      .then((rooms) => res.json(rooms))
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  };
+
+  update = (req, res) => {
+    const { id } = req.params;
+    Room.findOneAndUpdate(
+      {
+        _id: id,
+      },
+      req.body,
+      { new: true }
+    )
+      .then((updatedRoom) => res.json(updatedRoom))
+      .catch((err) => res.status(400).send(err));
+  };
 
 }
 
